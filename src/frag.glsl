3d99ad85c3d9ef60dff892b13058266db05a7b7e
acou12 <51276518+acou12@users.noreply.github.com>
2023-06-09 09:16:48 -0400

    Randomness

--- src/frag.glsl
+++ src/frag.glsl
@@ -9,20 +9,11 @@ in vec3 v_color;
 out vec4 outColor;
 
 void main() {
-    float slices = 20.0;
-    float angleSlices = 30.0;
-    float dist = distance(vec2(0.0, 0.0), v_position);
-    float dispDist = dist - v_time / 5.0;
-    float angle = atan(v_position.y, v_position.x);
-    float distSnapped = floor(dispDist * slices) / slices;
-    float angleSnapped = floor((angle + (dist - 0.5) * v_time) / 6.28318 * angleSlices) * 6.28318 / angleSlices;
-    // float distSnapped = dist;
-    vec3 color = (sin(vec3(
-        distSnapped * 31.4 / 1.6 + v_time + angleSnapped * 3.0 * (mod(distSnapped * slices, 3.0) + 1.0),
-        distSnapped * 31.4 / 1.2 + v_time,
-        distSnapped * 31.4 / 2.1 + v_time
-    )) + 1.0) / 2.0;
+    float res = 100.0;
+    vec2 new_position = v_position;
+    new_position.x += sin(v_time) * (mod(floor(v_position.y * res), 2.0) * 2.0 - 1.0);
+    float rand = round(fract(sin(dot(floor((new_position.xy) * res) / res,vec2(12.1,78.233)))*(10000.0 + v_time)));
     outColor = vec4(
-        color,
+        vec3(rand),
     1.0);
 }
